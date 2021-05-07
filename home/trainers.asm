2629f0a0019fd4133e809c582484514088015e37
Rena Kunisaki <hyperhacker@gmail.com>
2021-05-06 20:21:57 -0400

    restore trainer lose text

--- home/trainers.asm
+++ home/trainers.asm
@@ -229,16 +229,6 @@ CheckTrainerFlag:: ; unreferenced
 	ret
 
 PrintWinLossText::
-	ld a, [wBattleType]
-	cp BATTLETYPE_CANLOSE
-	; code was probably dummied out here
-	jr .canlose
-
-; unused
-	ld hl, wWinTextPointer
-	jr .ok
-
-.canlose
 	ld a, [wBattleResult]
 	ld hl, wWinTextPointer
 	and $f ; WIN?
